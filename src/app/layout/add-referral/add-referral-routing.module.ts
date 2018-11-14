ecf919ec4bc4076669cbb03df2052be6146ae4c9
Biswajit <biswajit.manna@shyamfuture.com>
2018-11-14 15:15:35 +0530

    menu fixing

---
+++ src/app/layout/add-referral/add-referral-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddReferralComponent } from './add-referral.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AddReferralComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AddReferralRoutingModule { }
--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ReferralService } from "../../core/services/referral.service";
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,117 +6,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  currentLat: number;
-  currentLong: number;
-  companylist: any = [];
-  programList: any = [];
-  form: FormGroup;
-  userId: number;
+ 
   constructor(
-    private formBuilder: FormBuilder,
-    private referralService: ReferralService,
-    private toastr: ToastrService,
-    private router: Router
+   
   ) { }
 
   ngOnInit() {
-    this.currentLat = +localStorage.getItem('currentLat');
-    this.currentLong = +localStorage.getItem('currentLong');
-    this.userId = +localStorage.getItem('userId');
-    this.getCompanylist();
-    this.form = this.formBuilder.group({
-      referral_link: ["", Validators.required],
-      user_id: [this.userId, Validators.required],
-      company_id: ["", Validators.required],
-      program_id: ["", Validators.required],
-      description: [""],
-      lattitude: [""],
-      longitude: [""],
-    });
-  }
-
-  getCompanylist() {
-    this.referralService.getCompanyLst().subscribe(
-      res => {
-        console.log(res);
-        this.companylist = res['result'];
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  }
-
-  companyChange(id) {
-    if (id != "") {
-      this.getProgrammeList(id)
-    }
-    else {
-      this.programList = [];
-    }
-  }
-
-  getProgrammeList(id) {
-    this.referralService.getProgramListByCompany(id).subscribe(
-      res => {
-        console.log(res);
-        this.programList = res['result'];
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  }
-
-  addReferral() {
-    console.log(this.form.value)
-    if (this.form.valid) {
-      if (this.currentLat == undefined && this.currentLong == undefined) {
-        this.toastr.error("Please allow your location", '', {
-          timeOut: 3000,
-        });
-      }
-      else {
-        this.form.patchValue({
-          lattitude: this.currentLat,
-          longitude: this.currentLong
-        })
-        this.referralService.addReferral(this.form.value).subscribe(
-          res => {
-            console.log(res);
-            this.toastr.success("Referral code has been added successfully", '', {
-              timeOut: 3000,
-            });
-            this.router.navigate(['/']);
-          },
-          error => {
-            console.log(error)
-          }
-        )
-      }
-    } else {
-      this.markFormGroupTouched(this.form)
-    }
-  }
-
-  markFormGroupTouched(formGroup: FormGroup) {
-    (<any>Object).values(formGroup.controls).forEach(control => {
-      control.markAsTouched();
-      if (control.controls) {
-        control.controls.forEach(c => this.markFormGroupTouched(c));
-      }
-    });
-  }
-
-  isFieldValid(field: string) {
-    return !this.form.get(field).valid && (this.form.get(field).dirty || this.form.get(field).touched);
-  }
-
-  displayFieldCss(field: string) {
-    return {
-      'is-invalid': this.form.get(field).invalid && (this.form.get(field).dirty || this.form.get(field).touched),
-      'is-valid': this.form.get(field).valid && (this.form.get(field).dirty || this.form.get(field).touched)
-    };
-  }
+    
+  } 
 
 }
